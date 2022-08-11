74e4c31e588b9303c2cb12578f89c9fa2a67fa81
Martin Nforne <wnforne@gmail.com>
2022-08-11 16:58:10 -0400

    cli_game_rock_paper_scissors

---
+++ main.py
@@ -0,0 +1,86 @@
+# askpython.com
+
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_images = [rock, paper, scissors]
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(game_images[user_choice])
+
+computer_choice = random.randint(0, 2)
+print("Computer chose:")
+if user_choice >= 3 or user_choice < 0:
+    print("You typed an invalid number, you lose!")
+else:
+    print(game_images[computer_choice])
+    if user_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif computer_choice == 0 and user_choice == 2:
+        print("You lose")
+    elif computer_choice > user_choice:
+        print("You lose")
+    elif user_choice > computer_choice:
+        print("You win!")
+    elif computer_choice == user_choice:
+        print("It's a draw")
+
+####### Debugging challenge: #########
+# Try running this code and type 5.
+# It will give you an IndexError and point to line 32 as the issue.
+# But on line 38 we are trying to prevent a crash by detecting
+# any numbers great than or equal to 3 or less than 0.
+# So what's going on?
+# Can you debug the code and fix it?
+# Solution: https://repl.it/@appbrewery/rock-paper-scissors-debugged-end
+
+# -------------------------------------------------------------------------
+
+import random
+import module
+# import fileinput
+print(module.pi)
+
+rand_int = random.randint(1, 3)
+rand_flt = random.random() * 5
+# in_put = fileinput.input()
+
+# -------------------------------------------------------------------------
+
+options = ['rock', 'paper', 'scissors']
+
+user_input = input('Enter 1 for rock, 2 for paper or 3 for scissors')
+comp_input = random.randint(1, 3)
+
+print(f'user {int(user_input)}', options[int(user_input) - 1])
+print(f'comp {int(comp_input)}', options[int(comp_input) - 1])
+
+if int(user_input) == int(comp_input):
+    print("Y W")
+    
